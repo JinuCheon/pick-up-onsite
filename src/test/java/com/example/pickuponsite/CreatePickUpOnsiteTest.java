c704198911d5dd35f08c018e64fd4c6872af29e9
JinuCheon <woojin8787@gmail.com>
2024-12-20 21:13:01 +0900

    add todolist for testing

---
+++ src/test/java/com/example/pickuponsite/CreatePickUpOnsiteTest.java
@@ -0,0 +1,11 @@
+package com.example.pickuponsite;
+
+/**
+ * 커머스에서 결제를 완료한 고객을 대상으로, 현장수령을 등록합니다.
+ *
+ * @checklist - [ ] 배송번호 바코드를 입력하여 현장수령을 등록한다
+ * - [ ] 주문 이력이 없다면 현장수령을 등록할 수 없다
+ * - [ ] 이미 현장수령이 등록된 주문은 현장수령을 등록할 수 없다
+ */
+public class CreatePickUpOnsiteTest {
+}
