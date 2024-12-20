2fa738b0119b31220cb2668257c7f66570d91723
JinuCheon <woojin8787@gmail.com>
2024-12-20 23:29:34 +0900

    add fail test: 체크리스트에서 다음 것으로 이동: 주문 이력이 없으면 현장수령 불가능

--- src/test/java/com/example/pickuponsite/CreatePickUpOnsiteTest.java
+++ src/test/java/com/example/pickuponsite/CreatePickUpOnsiteTest.java
@@ -11,12 +11,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * 커머스에서 결제를 완료한 고객을 대상으로, 현장수령을 등록합니다.
  *
  * @checklist
  * - [v] 배송번호 바코드를 입력하여 현장수령을 등록한다
- * - [ ] 주문 이력이 없다면 현장수령을 등록할 수 없다
+ * - [v] 주문 이력이 없다면 현장수령을 등록할 수 없다
  * - [ ] 이미 현장수령이 등록된 주문은 현장수령을 등록할 수 없다
  */
 public class CreatePickUpOnsiteTest {
@@ -124,6 +126,7 @@ public class CreatePickUpOnsiteTest {
 
     private class CommerceOrderRepository {
         public CommerceOrder getBy(final String deliverySequence) {
+//            assert !Objects.equals(deliverySequence, "illegalDeliverySequence") : "No delivery sequence";
             return new CommerceOrder(
                     1L,
                     deliverySequence,
